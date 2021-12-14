afcf4afd4aafe90fc19244eacc926f0791830de1
aasilbek <asilbek.aliev.1999@mail.ru>
2021-12-14 12:18:12 +0500

    saving to db chats

--- src/chat/routing.py
+++ src/chat/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 
-from .consumer import ChatConsumer
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer.as_asgi()),
